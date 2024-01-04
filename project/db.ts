126aa7bcb7f0964f7a843c2ac56c6f46e8c0b3a0
Ghost6818 <96416735+Ghost6818@users.noreply.github.com>
2024-01-04 13:42:04 +0100

    final-db

--- project/db.ts
+++ project/db.ts
@@ -3,83 +3,33 @@ import { UrlObject } from "expo-router/build/LocationProvider";
 const db: Db = {
   organic: [
     {
-      title: "29 sposobów na zarobienie dodatkowych pieniędzy - GeekWork",
-      link: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+      title: "Oszczędzaj podróżując ",
+      link: "https://www.trustedhousesitters.com/",
       snippet:
-        "„Być może dzisiaj nie masz długów, dobrze zarabiasz i myślisz sobie, że nie potrzebujesz oszczędności, ale zastanów się, bo być może od zaciągania kredytów ...",
+        "Co to takiego? jest to globalna zweryfikowana strona, gdzie ludzie z całego świata dodają ogłoszenia, że wyjeżdzają na jakiś czas i potrzebują kogoś kto zaopiekuję się ich domem i zwierzętami. W zamian możesz mieszkać w ich domu całkowicie za darmo. Przez ile? Od wypadu na weekend, tydzień po miesiąc lub więcej. Tak się właśnie znalazłam na Malcie, mieszkam za darmo a w zamian opiekuje się cudownymi psiakami i kotkami.",
+      date: "15 września 2023",
       position: 1,
     },
     {
-      title: "Zarabianie pieniędzy - 4 główne sposoby - KobietaZarabia.com",
-      link: "https://kobietazarabia.com/zarabianie-pieniedzy-4-sposoby/",
+      title: "Marketing w social mediach",
+      link: "https://joankastefaniak.pl/newsletter/",
       snippet:
-        "Dla Ciebie to jest łatwe i oczywiste, rozwiązujesz i bierzesz za to rozwiązanie pieniądze. ... Zarabiasz pieniądze nie wkładając w to swojego ...",
-      date: "21 paź 2022",
+        "Social media kreują rzeczywistość nie tylko  w Internecie. Wyznaczają trendy, ukierunkowują reklamy, tworzą standardy zachowań i swoim zasięgiem budują zaangażowane społeczności. Dzięki nim możesz zaistnieć w przestrzeni marketingowej w świadomości swoich odbiorców i zbudować z nimi trwałe i wartościowe relacje.",
       position: 2,
     },
     {
-      title: "JAK ZARABIAĆ W INTERNECIE - 10 NAJLEPSZYCH METOD",
-      link: "https://pomysl-na-milion.pl/jak-zarabiac-w-internecie/",
+      title: "Zarabiaj jako grafik",
+      link: "https://www.fiverr.com/",
       snippet:
-        "Nie każdy musi zarabiać drugą wypłatę i raczej oczywiste, że nie zostaniesz dzięki temu milionerem. ... Zarabianie tym sposobem nie należy do najbardziej ...",
+        "Fiverr to serwis ze zleceniami dla freelancerów z całego świata. Jego idea opiera się na ofertach (gigs), które freelancer opracowuje dla potencjalnych zleceniodawców. Może to być np. przygotowanie loga.",
       position: 3,
     },
     {
-      title:
-        "Poradnik dla początkujących: jak zarabiać na YouTubie, gdy nie masz czasu - YouTube",
-      link: "https://youtube.com/watch?v=UvCjxw0ESR4",
-      snippet: "... oczywiste sposoby zarabiania 4:13 Programy ...",
-      date: "8 wrz 2020",
+      title: "Poradnik dla początkujących: jak zarabiać na Spotify",
+      link: "https://playlistpush.com/",
+      snippet:
+        "Playlist Push jest legalny i płaci swoim użytkownikom. Otrzymasz wynagrodzenie za recenzowanie utworów i dodawanie ich do swojej listy odtwarzania i możesz zarobić do $12 za recenzję. Strona płaci tylko za pośrednictwem przelewu bankowego.",
       position: 4,
-    },
-    {
-      title: "5 sposobów na zarabianie w sieci - EDULIDER.pl",
-      link: "https://www.edulider.pl/biznes/5-sposobow-na-zarabianie-w-sieci",
-      snippet:
-        "1. Zarabianie w sieci jest dla każdego. Wiele osób wychodzi z błędnego założenia, że w pracy przez Internet duże zyski może osiągnąć każdy. Prawda jest jednak ...",
-      position: 5,
-    },
-    {
-      title:
-        "Jak zarabiać? Primo: Nie bądź jeleniem! To śledztwo pokaże Ci, jak nie dać się oszukać",
-      link: "https://bithub.pl/artykuly/jak-zarabiac-primo-nie-badz-jeleniem-to-sledztwo-pokaze-ci-jak-nie-dac-sie-oszukac/",
-      snippet:
-        "Oczywistą oczywistością jest w tym względzie to, że gdybyś je miał, nie musiałbyś korzystać z żadnych sygnałów. Sygnały mają swój procent ...",
-      date: "11 mar 2022",
-      position: 6,
-    },
-    {
-      title:
-        "6 sposobów na zarabianie w mediach społecznościowych - RekinySukcesu.pl",
-      link: "https://www.rekinysukcesu.pl/blog/social-media/6-sposobow-na-zarabianie-w-mediach-spolecznosciowych",
-      snippet:
-        "Znamy oczywistą istotę mediów ... Jeżeli nigdy wcześniej nie używałeś mediów społecznościowych, nie zniechęcaj się do nauki nowej umiejętności.",
-      date: "12 sie 2022",
-      position: 7,
-    },
-    {
-      title:
-        "Opowieść o trudnej sztuce zarabiania, czyli proste sekrety drogi do dużych pieniędzy",
-      link: "https://jakoszczedzacpieniadze.pl/trudna-sztuka-zarabiania",
-      snippet:
-        "Jak maniak powtarzam, że najlepszym sposobem oszczędzania jest zarabianie pieniędzy. Ale nie uważam pieniędzy za najważniejszy cel pracy.",
-      date: "30 mar 2017",
-      position: 8,
-    },
-    {
-      title:
-        "Jak zarabiać na bankach – kontach bankowych, kartach i innych produktach? Poradnik - Moniaki.pl",
-      link: "https://moniaki.pl/co-zrobic-aby-zarabiac-na-bankach-zamiast-one-na-nas/",
-      snippet:
-        "Nigdy nie pracowałem w banku, ale obserwując od lat rynek bankowy śmiem twierdzić, że banki nie zarabiają na kontach osobistych. ... Oczywistym jest to że nic za ...",
-      position: 9,
-    },
-    {
-      title: "Czy zarabianie w internecie jest łatwe? - Get Paid 2.0",
-      link: "https://getpaid20.pl/czy-zarabianie-w-internecie-jest-latwe/",
-      snippet:
-        "Opinia na temat łatwego i szybkiego zarabiania pieniędzy przez internet. Jak zarabiać w internecie i pracować w domu w prosty sposób.",
-      position: 10,
     },
   ],
   peopleAlsoAsk: [
@@ -120,39 +70,27 @@ const db: Db = {
   Category: [
     {
       index: 1,
-      logo: "https://images.ctfassets.net/6dczv5fypa6q/39ovXkWsNxXm0OyFJ6uoYw/e02f63d27eeed96f376dec5188ccaf36/world-nomads-travel-insurance.png",
-      name: "World Nomads",
+      logo: "https://europe1.discourse-cdn.com/business20/uploads/trustedhousesitters/original/2X/6/60bcbd843fd6de38bc5fce4be0d8a3c597a2d8ee.png",
+      name: "TrustedHousesitter",
       query: "Zarabiaj podróżójąc.",
     },
     {
       index: 2,
       logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-      name: "Zarabiaj poprzez testowanie gier!",
-      query: "Gry",
+      name: "Promój i zarabiaj!",
+      query: "social-media marketing",
     },
     {
       index: 3,
-      logo: "https://i.pngimg.me/thumb/f/720/126da8bbcf.jpg",
-      name: "Ucz i zarabiaj!",
-      query: "AI",
+      logo: "https://seeklogo.com/images/F/fiverr-new-2020-logo-354E8A08FD-seeklogo.com.png",
+      name: "Fiverr",
+      query: "Grafik designer",
     },
     {
       index: 4,
-      logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-      name: "Podziel się opinią i zarabiaj!",
-      query: "Rezenzje",
-    },
-    {
-      index: 5,
       logo: "https://playlistpush.com/blog/content/images/2023/02/playlistpush-icon-red-2.png",
       name: "Playlistpush",
       query: "Podziel się muzyką!",
-    },
-    {
-      index: 6,
-      logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-      name: "TestApp",
-      query: "Testowanie aplikacji",
     },
   ],
 };
